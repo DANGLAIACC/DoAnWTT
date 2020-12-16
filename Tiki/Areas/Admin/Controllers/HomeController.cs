13c42416299d310b4cb897bd2478b44126a5d34e
may tmt <laidq@tmt.vn>
2020-12-16 09:31:08 +0700

    change dashboard

--- Tiki/Areas/Admin/Controllers/HomeController.cs
+++ Tiki/Areas/Admin/Controllers/HomeController.cs
@@ -13,5 +13,10 @@ namespace Tiki.Areas.Admin.Controllers
         {
             return View();
         }
+        public ActionResult Login()
+        {
+            return View();
+        }
+
     }
 }
