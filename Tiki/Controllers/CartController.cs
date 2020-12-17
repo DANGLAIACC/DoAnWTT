41440afa4a7b2a82a886dadca466a1601cbbed78
danglaiacc <luubaonlai@gmail.com>
2020-12-17 23:54:36 +0700

    lam them gio hang

---
+++ Tiki/Controllers/CartController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tiki.Models;
+
+namespace Tiki.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View();
+        }
+        private TIKIContext _db = new TIKIContext();
+
+        /// <summary>
+        /// Add to cart
+        /// </summary>
+        /// <param name="book_id"></param>
+        /// <returns></returns>
+        public ActionResult Add(int book_id)
+        {
+            if (Session["cart"] == null)
+            {
+                List<CartModel> cart = new List<CartModel>();
+                cart.Add(new CartModel(_db.BOOKs.Find(book_id), 1));
+                Session["cart"] = cart;
+            }
+            else
+            {
+                List<CartModel> cart = (List<CartModel>)Session["cart"];
+                int index = findBookId(book_id);
+                // chưa tồn tại trong giỏ hàng
+                if (index == -1)
+                    cart.Add(new CartModel(_db.BOOKs.Find(book_id), 1));
+                else
+                    cart[index].Quantity++;
+                Session["cart"] = cart;
+            }
+            return View("cart");
+        }
+        public ActionResult Remove(int book_id)
+        {
+            int index = findBookId(book_id);
+            List<CartModel> cart = (List<CartModel>)Session["cart"];
+            if (index != -1)
+                cart.RemoveAt(index);
+            Session["cart"] = cart;
+            return View("cart");
+        }
+        /// <summary>
+        /// Tìm mã sách trong danh sách giỏ hàng ở session
+        /// </summary>
+        /// <param name="book_id">mã sách cần tìm</param>
+        /// <returns>-1 nếu ko thấy</returns>
+        private int findBookId(int book_id)
+        {
+            List<CartModel> cart = (List<CartModel>)Session["cart"];
+            for (int i = 0; i < cart.Count; i++)
+            {
+                if (cart[i].b.book_id == book_id)
+                    return i;
+            }
+            return -1;
+        } 
+    }
+}
---
+++ Tiki/Models/CartModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tiki.Models
+{
+    public class CartModel
+    {
+        public BOOK b = new BOOK();
+        public int Quantity { get; set; }
+
+        public CartModel(BOOK b, int quantity)
+        {
+            this.b = b;
+            this.Quantity = quantity;
+        }
+        public CartModel() { }
+
+    }
+}
