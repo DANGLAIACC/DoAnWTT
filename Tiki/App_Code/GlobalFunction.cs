8909554337f74a61105428e7a8e3186d2b2c94ec
may tmt <laidq@tmt.vn>
2020-12-17 17:02:44 +0700

    lam so trang o detail

--- Tiki/App_Code/GlobalFunction.cs
+++ Tiki/App_Code/GlobalFunction.cs
@@ -11,8 +11,17 @@ namespace Tiki.App_Code
         {
             return string.Format("{0:#,##0}đ", price).Replace(",", ".");
         }
-        public static string RenderStar(int count)
+        public static string RenderStarByListEva(ICollection<EVALUATE> lst)
         {
+            int tong = 0;
+            foreach (var e in lst)
+                tong += e.eva_rate;
+
+            return RenderStarByCount((int)Math.Floor(tong * 1.0 / lst.Count));
+        }
+        public static string RenderStarByCount(int count)
+        {
+
             string result = "";
             for (int i = 0; i < count; i++)
             {
@@ -20,7 +29,7 @@ namespace Tiki.App_Code
             }
             for (int i = 0; i < 5 - count; i++)
             {
-                result += "<i class='icomoon icomoon-star-2'></i>";
+                result += "<i class='icomoon icomoon-star dark'></i>";
             }
             return result;
         }
